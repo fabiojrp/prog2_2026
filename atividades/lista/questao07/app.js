f74933b25727cd87e647aad6fa08e3e8ae81a5ef
fabiojrp <fabio.pinheiro@ifc.edu.br>
2026-08-17 14:12:02 -0300

    ok

---
+++ atividades/lista/questao07/app.js
@@ -0,0 +1,27 @@
+// Fazer uma aplicação que simule um login, digitando email e senha. Direcionar
+// para uma página principal, caso esteja correto, ou voltar para a página inicial
+// (login), caso digite os valores inválidos
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+const usuarioCorreto = "admin";
+const senhaCorreta = "123654";
+
+app.get("/", (req, res)=>{
+    res.render("login");
+});
+
+app.post("/efetuarLogin", (req, res)=>{
+    const {usuario, senha} = req.body;
+
+    if (usuario === usuarioCorreto && senha === senhaCorreta){
+        res.render("principal", {usuario});
+    }else{
+        res.render("login");
+    }
+});
+
+app.listen(port);
---
+++ atividades/lista/questao09/app.js
@@ -0,0 +1,23 @@
+// Fazer cadastro de filmes, informando: título, ano de lançamento e estilo (ação,
+// aventura, romance, etc) em um <select>. Ao exibir os filmes cadastrados,
+// organizar por estilo. Exemplo: exibir todos os filmes de ação, depois todos os de
+// aventura, depois todos os filmes de romance, etc.
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+const filmes = [];
+
+app.get("/", (req, res)=>{
+    res.render("filmes", { filmes });
+});
+
+app.post("/", (req, res)=>{
+    const { titulo, ano, estilo } = req.body;
+    filmes.push({ titulo, ano, estilo });
+    res.redirect("/");
+});
+
+app.listen(port);
