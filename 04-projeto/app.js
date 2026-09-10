b67a7dbedee9a605fef3e183c8b35222ea877e6b
fabiojrp <fabio.pinheiro@ifc.edu.br>
2026-09-10 09:36:22 -0300

    ok

--- 04-projeto/app.js
+++ 04-projeto/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
-
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
