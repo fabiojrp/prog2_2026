efe603188b416be82d66e5488b7fca523e76cfbc
fabiojrp <fabio.pinheiro@ifc.edu.br>
2026-09-15 17:05:17 -0300

    ok

--- atividades/03-bd/01-produtos/app.js
+++ atividades/03-bd/01-produtos/app.js
@@ -1,23 +1,17 @@
-/*
-Fazer um cadastro de produtos que gere um relatório de “baixo estoque”. Além
-do nome do produto, o usuário deve fornecer o valor e a quantidade daquele
-produto. Ao exibir os produtos cadastrados, devem aparecer somente os
-produtos com quantidade menor que 10.
-*/
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 let produtos = [];
 app.get('/', (req, res) => {
-  res.render('index', { produtos});
+  res.render('form-cadastro.ejs', { produtos});
 });
 
 app.post('/cadastrar', (req, res) => {
   produtos.push(req.body);
-  // Aqui você pode adicionar o produto a uma lista ou banco de dados
-  res.render('index', { produtos });
+  res.render('lista-produtos.ejs', { produtos });
 });
 
 app.listen(3000);
